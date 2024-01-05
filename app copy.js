961c7633ca0d04fb4145a74f6d1e2d67e518d3ed
M. Sertaç Toröz <msertactoroz@gmail.com>
2024-01-05 12:06:23 +0300

    cherio

--- app copy.js
+++ app copy.js
@@ -1,84 +1,84 @@
 
-//MongoDB boilerplate - direct copy
+// //MongoDB boilerplate - direct copy
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://sertactoroz:<password>@cluster0.y6rtf7p.mongodb.net/?retryWrites=true&w=majority";
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
+// // const { MongoClient, ServerApiVersion } = require('mongodb');
+// // const uri = "mongodb+srv://sertactoroz:<password>@cluster0.y6rtf7p.mongodb.net/?retryWrites=true&w=majority";
+// // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// // const client = new MongoClient(uri, {
+// //   serverApi: {
+// //     version: ServerApiVersion.v1,
+// //     strict: true,
+// //     deprecationErrors: true,
+// //   }
+// // });
+// // async function run() {
+// //   try {
+// //     // Connect the client to the server	(optional starting in v4.7)
+// //     await client.connect();
+// //     // Send a ping to confirm a successful connection
+// //     await client.db("admin").command({ ping: 1 });
+// //     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+// //   } finally {
+// //     // Ensures that the client will close when you finish/error
+// //     await client.close();
+// //   }
+// // }
+// // run().catch(console.dir);
+
+// /////////////// OPENAI 
+
+// const axios = require('axios');
+// const mongoose = require('mongoose');
+// module.exports = async () => {
+//     try {
+//         await // MongoDB Bağlantısı
+//             mongoose.connect('mongodb+srv://sertactoroz:ly0ubaUYD4ymqNja@cluster0.y6rtf7p.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+//         ;
+//         console.log("CONNECTED TO DATABASE SUCCESSFULLY");
+//     } catch (error) {
+//         console.error('COULD NOT CONNECT TO DATABASE:', error.message);
+//     }
+// };
+// // MongoDB Bağlantısı
+// // mongoose.connect('mongodb+srv://sertactoroz:<password>@cluster0.y6rtf7p.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+// // mongoose.connect('mongodb+srv://sertactoroz:ly0ubaUYD4ymqNja@cluster0.y6rtf7p.mongodb.net/?retryWrites=true&w=majority');
+
+// // Veritabanı Şeması
+// const apkSchema = new mongoose.Schema({
+//     versionId: String,
+//     releaseDate: Date,
+//     variants: [{
+//         variantId: String,
+//         architecture: String,
+//         minAndroidVersion: String,
+//         dpi: String
+//     }]
 // });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
+
+// const Apk = mongoose.model('Apk', apkSchema);
+
+// // APK Bilgilerini Çekme Fonksiyonu
+// async function fetchApkInfo() {
+//     try {
+//         const response = await axios.get('https://www.apkmirror.com/?post_type=app_release&searchtype=apk&s=instagram/');
+//         // TODO: Response'dan gerekli bilgileri çekme
+//         // sourcery skip: use-object-destructuring
+//         const versionId = response.data.versionId;
+//         // sourcery skip: use-object-destructuring
+//         const releaseDate = response.data.releaseDate;
+//         // sourcery skip: use-object-destructuring
+//         const variants = response.data.variants;
+
+//         // TODO: MongoDB'ye kaydetme
+//         const apk = new Apk({ versionId, releaseDate, variants });
+//         await apk.save();
+//     } catch (error) {
+//         console.error('APK bilgileri çekilirken hata oluştu:', error.message);
+//     }
 // }
-// run().catch(console.dir);
 
-/////////////// OPENAI 
+// // Uygulama İlk Çalıştığında Fonksiyonu Çağırma
+// fetchApkInfo();
 
-const axios = require('axios');
-const mongoose = require('mongoose');
-module.exports = async () => {
-    try {
-        await // MongoDB Bağlantısı
-            mongoose.connect('mongodb+srv://sertactoroz:ly0ubaUYD4ymqNja@cluster0.y6rtf7p.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-        ;
-        console.log("CONNECTED TO DATABASE SUCCESSFULLY");
-    } catch (error) {
-        console.error('COULD NOT CONNECT TO DATABASE:', error.message);
-    }
-};
-// MongoDB Bağlantısı
-// mongoose.connect('mongodb+srv://sertactoroz:<password>@cluster0.y6rtf7p.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect('mongodb+srv://sertactoroz:ly0ubaUYD4ymqNja@cluster0.y6rtf7p.mongodb.net/?retryWrites=true&w=majority');
-
-// Veritabanı Şeması
-const apkSchema = new mongoose.Schema({
-    versionId: String,
-    releaseDate: Date,
-    variants: [{
-        variantId: String,
-        architecture: String,
-        minAndroidVersion: String,
-        dpi: String
-    }]
-});
-
-const Apk = mongoose.model('Apk', apkSchema);
-
-// APK Bilgilerini Çekme Fonksiyonu
-async function fetchApkInfo() {
-    try {
-        const response = await axios.get('https://www.apkmirror.com/?post_type=app_release&searchtype=apk&s=instagram/');
-        // TODO: Response'dan gerekli bilgileri çekme
-        // sourcery skip: use-object-destructuring
-        const versionId = response.data.versionId;
-        // sourcery skip: use-object-destructuring
-        const releaseDate = response.data.releaseDate;
-        // sourcery skip: use-object-destructuring
-        const variants = response.data.variants;
-
-        // TODO: MongoDB'ye kaydetme
-        const apk = new Apk({ versionId, releaseDate, variants });
-        await apk.save();
-    } catch (error) {
-        console.error('APK bilgileri çekilirken hata oluştu:', error.message);
-    }
-}
-
-// Uygulama İlk Çalıştığında Fonksiyonu Çağırma
-fetchApkInfo();
-
-// Dışardan Tetiklenebilen Fonksiyon
-exports.fetchAndSaveApkInfo = fetchApkInfo;
+// // Dışardan Tetiklenebilen Fonksiyon
+// exports.fetchAndSaveApkInfo = fetchApkInfo;
--- fetchData.js
+++ fetchData.js
@@ -1,11 +1,27 @@
-// fetchData.js
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 async function fetchInstagramAPKVersions() {
   try {
-    const response = await axios.get('https://www.apkmirror.com/apk/instagram/instagram/');
-    // Parse the data and extract the necessary information
-    // Save data to MongoDB
+    const response = await axios.get('https://www.apkmirror.com/uploads/?appcategory=instagram-instagram');
+    const $ = cheerio.load(response.data);
+
+    // Extract information from the HTML structure
+    const versions = [];
+    $('article').each((index, element) => {
+      const version = {
+        name: $(element).find('.fontBlack').text().trim(),
+        downloadLink: $(element).find('.downloadButton').attr('href'),
+        // Add other fields as needed
+      };
+      versions.push(version);
+    });
+
+    // Log the extracted data
+    console.log('Extracted Versions:', versions);
+
+    // Save data to MongoDB (you need to implement this part)
+    // saveToMongoDB(versions);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
